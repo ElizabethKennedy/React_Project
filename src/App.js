b2a9f3721cd60146fdd1ef6febea8b42ab55c6dc
Elizabeth Kennedy <eakennedy.2412@gmail.com>
2022-09-05 19:54:45 -0700

    completed class exercise

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react'; 
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,9 +7,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1> My First React App</h1>
+        <p>This is new and exciting terrain we are exploring!</p>
+        <p>Here is an overview of our introduction to working with React.</p>
+        <h2>Takeaways from Intro to React:</h2>
+        <ul>
+          <li>React is a JavaScript Library for creating <b>dynamic</b> single-page applications.</li>
+          <li>Components are the building blocks of an application, that return jsx or JavaScript xml.</li>
+          <li>The ReactDOM is the <i>virtual dom</i> that manages and syncs updates with the real browser DOM.</li>
+        </ul>
+
         <a
           className="App-link"
           href="https://reactjs.org"
